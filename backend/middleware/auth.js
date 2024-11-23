a85a1684cda2ffc522253b11133328fb177ea438
Aghilas AZZOUG <azzougaghiles@hotmail.com>
2024-11-23 14:04:41 +0100

    fix bug: authorization jwt

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,13 +2,17 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
     try {
-        const token = res.headers.autorization.split("")[1]; // recuperer le token, on recupere le header, on divise chaine de caracete autour d el'espace, entre barrer et token, on récupere le seocnd donc index 1
-        const decoderToken = jwt.verify(token, "RANDOM_TOKEN_SECRET"); //onecodele token
-        const userId = decoderToken.userId;
-        req.auth = {
-            userId: userId,
-        };
+        const token = req.headers.authorization.split(" ")[1]; // Extraire le token de l'en-tête Authorization
+        const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET"); // Vérifier la validité du token
+        const userId = decodedToken.userId; // Extraire l'ID de l'utilisateur à partir du token
+        req.auth = { userId: userId }; // Attacher l'ID de l'utilisateur à l'objet req
+        next(); // Passer à la prochaine étape
     } catch (error) {
-        res.status(401).json({ error });
+        res.status(401).json({ error: "Invalid or missing token" }); // Si le token est invalide ou manquant
     }
 };
+
+/*rappel:
+-La méthode verify() du package jsonwebtoken permet de vérifier la validité d'un token (sur une requête entrante, par exemple).
+-Ajoutez bien votre middleware d'authentification dans le bon ordre sur les bonnes routes.
+-Attention aux failles de sécurité !*/
--- backend/models/Things.js
+++ backend/models/Things.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 //schéma de données, on tutilise la méthode Schema de mongoose
 //pas besoin de champs id car automatisé par mongoose
 
+/*
+-La méthode  Schema  de Mongoose vous permet de créer un schéma de données pour votre base de données MongoDB.
+-La méthode  model  transforme ce modèle en un modèle utilisable.
+*/
+
 const thingSchema = mongoose.Schema({
     //La méthode  Schema  de Mongoose vous permet de créer un schéma de données pour votre base de données MongoDB.
     title: { type: String, required: true },
